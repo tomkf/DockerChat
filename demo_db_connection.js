895d6ef78809130bd664d55be9a882034acf605c
Thomas Ferris <thomaskeithferris@gmail.com>
2019-07-28 14:42:47 -0400

    update, new packages and server files

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -1,3 +1,32 @@
+//DOCKER notes:
+
+// FROM node:10
+
+// # Create app directory
+// WORKDIR /usr/src/app
+
+// # Install app dependencies
+// # A wildcard is used to ensure both package.json AND package-lock.json are copied
+// # where available (npm@5+)
+// COPY package*.json ./
+
+// RUN npm install
+// # If you are building your code for production
+// # RUN npm ci --only=production
+
+// # Bundle app source
+// COPY . .
+
+// EXPOSE 8080
+// CMD [ "node", "server.js" ]
+
+// You will also want a .dockerignore file
+// with:
+// node_modules
+// npm-debug.log
+
+////////////////////////////////////////////////////////////////////////////
+
 // const mysql = require("mysql");
 // const connection = mysql.createConnection({
 //   host: "127.0.0.1",
--- src/App.js
+++ src/App.js
@@ -30,28 +30,3 @@ class App extends Component {
 }
 
 export default App;
-
-// FROM node:10
-
-// # Create app directory
-// WORKDIR /usr/src/app
-
-// # Install app dependencies
-// # A wildcard is used to ensure both package.json AND package-lock.json are copied
-// # where available (npm@5+)
-// COPY package*.json ./
-
-// RUN npm install
-// # If you are building your code for production
-// # RUN npm ci --only=production
-
-// # Bundle app source
-// COPY . .
-
-// EXPOSE 8080
-// CMD [ "node", "server.js" ]
-
-// You will also want a .dockerignore file
-// with:
-// node_modules
-// npm-debug.log
