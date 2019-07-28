37cd7a41114ef5a52da7b7b0e95b307fd5f92133
Thomas Ferris <thomaskeithferris@gmail.com>
2019-07-28 17:38:33 -0400

    components should interact now

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-import Message from "./Message";
+import Message from "./Comment";
 
-class MessageList extends Component {
-  render() {}
+class MessageList extends React.Component {
+  render() {
+    const displaymessages = this.props.messages.map(message => (
+      <Message name={message.name} content={message.content} date={date} />
+    ));
+    return <main className="messages">{displaymessages}</main>;
+  }
 }
+
+export default MessageList;
